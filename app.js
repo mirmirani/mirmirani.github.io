9a08f0338bdf4b0122b049ebdfd359996f6c605e
Mir Mirani <mir.mirani@outlook.com>
2020-09-07 11:39:18 +0100

    portfolio-update

---
+++ app.js
@@ -0,0 +1,9 @@
+//https://scotch.io/courses/getting-started-with-vuejs/vue-events-building-an-accordion
+
+const app = new Vue({
+    el: '#app',
+    data: {
+      showNav: false
+    }
+  });
+  
