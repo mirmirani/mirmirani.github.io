9f11eb0b905cb3dd0c01e394b3dcc7723f4f8658
Mir <57907512+mirmirani@users.noreply.github.com>
2021-10-14 00:47:55 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-//https://scotch.io/courses/getting-started-with-vuejs/vue-events-building-an-accordion
-
-const app = new Vue({
-    el: '#app',
-    data: {
-      showNav: false
-    }
-  });
-  
